a497c54647c9682a0aaa24fb82b505a7e15361b3
Joshpowerfit <nwankwojoshua100@gmail.com>
2023-01-24 13:26:14 -0800

    Sorting task

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -51,4 +51,24 @@ void max_heapify(int *array, size_t size, size_t base, size_t root)
  *             order using the heap sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
+ *
+ * Description: Implements the sift-down heap sort
+ * algorithm. Prints the array after each swap.
+ */
+void heap_sort(int *array, size_t size)
+{
+	int i;
 
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = (size / 2) - 1; i >= 0; i--)
+		max_heapify(array, size, size, i);
+
+	for (i = size - 1; i > 0; i--)
+	{
+		swap_ints(array, array + i);
+		print_array(array, size);
+		max_heapify(array, size, i, 0);
+	}
+}
